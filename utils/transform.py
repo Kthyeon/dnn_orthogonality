6fc9d890fca4e3b1eb0a42f3ed0139a62ae08eca
Kthyeon <potter32@kaist.ac.kr>
2020-07-22 14:46:19 +0900

    modify for tiny-image

--- utils/transform.py
+++ utils/transform.py
@@ -99,7 +99,7 @@ def data_setter(args, root = '/home/taehyeon/'):
         testset = datasets.CIFAR100(root + args.dataset + '/', train = False, transform = test_transforms, download = False)
     elif args.dataset == 'tiny-image':
         _trainset = datasets.ImageFolder(root + args.dataset + '/train', transform=train_transforms)
-        testset = dataset.ImageFolder(root + args.dataset + '/valid', transform=test_transforms)
+        testset = dataset.ImageFolder(root + args.dataset + '/val', transform=test_transforms)
     elif args.dataset == 'image':
         _trainset = datasets.ImageFolder(root + args.dataset + '/Data/train', transform=train_transforms)
         testset = dataset.ImageFolder(root + args.dataset + '/Data/valid', transform=test_transforms)
